efa15d09a47eff8b8b8e427158793863443e37d9
dosepehr <dosepehr@gmail.com>
2024-01-22 01:21:27 +0330

    modify tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        sm: { min: "500px" },
+        md: { min: "768px" },
+        lg: { min: "900px" },
+        xl: { min: "1200px" },
+      },
+      fontFamily: {
+        ralewayRegular: "Raleway-regular",
+        ralewayLight: "Raleway-light",
+        ralewayBold: "Raleway-bold",
+      },
+      colors: {
+        lightGreen: "#81C408",
+        mainYellow: "#FFb524",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
