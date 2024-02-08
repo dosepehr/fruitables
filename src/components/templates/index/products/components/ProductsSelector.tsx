713022a9a190c4521d166fb1d874f614f403fd0b
dosepehr <dosepehr@gmail.com>
2024-02-08 18:48:31 +0330

    edit Products Selector component

--- src/components/templates/index/products/components/ProductsSelector.tsx
+++ src/components/templates/index/products/components/ProductsSelector.tsx
@@ -1,14 +1,41 @@
 import { useState } from "react";
-import ProductSelectorButton from "./ProductSelectorButton";
 
 const ProductsSelector = () => {
+  const buttons: { id: number; content: string }[] = [
+    {
+      id: 1,
+      content: "All Products",
+    },
+    {
+      id: 2,
+      content: "Vegetables",
+    },
+    {
+      id: 3,
+      content: "Fruits",
+    },
+    {
+      id: 4,
+      content: "Bread",
+    },
+    {
+      id: 5,
+      content: "Meat",
+    },
+  ];
+  const [category, setCategory] = useState<string>("All Products");
+
   return (
     <div className="flex items-center justify-between space-x-4">
-      <ProductSelectorButton content="All Products" />
-      <ProductSelectorButton content="Vegetables" />
-      <ProductSelectorButton content="Fruits" />
-      <ProductSelectorButton content="Bread" />
-      <ProductSelectorButton content="Meat" />
+      {buttons.map((button) => (
+        <button
+          key={button.id}
+          onClick={() => setCategory(button.content)}
+          className={`rounded-full px-4 py-2 transition-all duration-300 ${button.content == category ? "bg-mainYellow text-white" : "bg-[#f4f6f8] text-[#45595b]"}`}
+        >
+          {button.content}
+        </button>
+      ))}
     </div>
   );
 };
