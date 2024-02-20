b1ad0c239f4a00caf71c6c8e81c0193a1193c86b
dosepehr <dosepehr@gmail.com>
2024-02-21 00:35:24 +0330

    edit responsive layout

--- src/components/modules/Footer/Footer.tsx
+++ src/components/modules/Footer/Footer.tsx
@@ -9,24 +9,24 @@ import {
 
 const Footer = () => {
   return (
-    <div className="bg-[#45595b] mt-10 py-4">
+    <div className="mt-10 bg-[#45595b] py-4">
       <Layout>
-        <div className="flex items-center justify-between">
-          <div>
+        <div className="flex flex-col items-center justify-between lg:flex-row">
+          <div className="self-start" >
             <p className="text-[2.5rem] text-lightGreen">Fruitables</p>
             <p className="text-mainYellow">Fresh products</p>
           </div>
-          <div className="relative w-1/2">
+          <div className="relative w-full lg:w-1/2 my-4 lg:my-0">
             <input
               type="text"
-              className="w-full rounded-full px-6 py-4"
+              className="w-full rounded-full px-4 py-4"
               placeholder="Your Email"
             />
-            <button className="absolute right-0 top-0 rounded-full bg-lightGreen px-6 py-4 font-semibold text-white">
+            <button className="absolute right-0 top-0 rounded-full bg-lightGreen px-4 py-4 font-semibold text-white">
               Subscribe Now
             </button>
           </div>
-          <div className="flex items-center justify-center gap-x-2">
+          <div className="flex items-center justify-center gap-x-2 self-end">
             <div className="rounded-full border border-mainYellow p-3 text-mainYellow">
               <FaTwitter />
             </div>
