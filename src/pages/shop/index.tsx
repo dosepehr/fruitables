c85bb1e7b4a1a1fb9ec9241c1782ec4caaf7e516
dosepehr <dosepehr@gmail.com>
2024-02-21 13:10:23 +0330

    complete shop page

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -2,20 +2,29 @@ import React from "react";
 import Breadcrumb from "@/components/modules/Breadcrumb/Breadcrumb";
 import Product from "@/components/templates/index/products/components/Product";
 import Layout from "@/components/modules/Layout/Layout";
+import Image from "next/image";
 
 const index = () => {
   return (
     <>
       <Breadcrumb />
       <Layout>
-        <div className="flex justify-center">
-          <div className="w-1/3">
+        <div className="flex flex-col justify-center lg:flex-row lg:space-x-5">
+          <div className="lg:w-1/4">
             <p className="text-[2.5rem] font-semibold text-darkGray">
               Fresh fruits shop
             </p>
-            <div></div>
+            <div>
+              <Image
+                className="mt-4 rounded-lg my-4"
+                src="/images/banner-fruits.jpg"
+                alt=""
+                width={1000}
+                height={1000}
+              />
+            </div>
           </div>
-          <div className="w-2/3">
+          <div className="lg:w-3/4">
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
               <Product />
               <Product />
