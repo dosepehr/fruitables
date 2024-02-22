5eb471487d17d7507c3c481d498a460e9cebea87
dosepehr <dosepehr@gmail.com>
2024-02-22 23:37:29 +0330

    add productDetail page

---
+++ src/pages/shop/[id].tsx
@@ -0,0 +1,14 @@
+import Breadcrumb from "@/components/modules/Breadcrumb/Breadcrumb";
+import { useRouter } from "next/router";
+
+const Product = () => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <Breadcrumb href="" text="" />
+    </div>
+  );
+};
+
+export default Product;
