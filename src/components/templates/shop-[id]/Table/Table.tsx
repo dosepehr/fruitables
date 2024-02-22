56c379f043df3a88bb6fd16f7fbbef81702f11bb
dosepehr <dosepehr@gmail.com>
2024-02-23 00:27:38 +0330

    add product table

---
+++ src/components/templates/shop-[id]/Table/Table.tsx
@@ -0,0 +1,20 @@
+const Table = () => {
+  return (
+    <div className="w-1/2 flex-col">
+      <div className="flex w-full items-center justify-between bg-[#f4f6f8] text-lightGray py-2">
+        <p className="w-1/2 text-center">Weight</p>
+        <p className="w-1/2 text-center">1 Kg</p>
+      </div>
+      <div className="flex w-full items-center justify-between text-lightGray py-2">
+        <p className="w-1/2 text-center">Weight</p>
+        <p className="w-1/2 text-center">1 Kg</p>
+      </div>
+      <div className="flex w-full items-center justify-between bg-[#f4f6f8] text-lightGray py-2">
+        <p className="w-1/2 text-center">Weight</p>
+        <p className="w-1/2 text-center">1 Kg</p>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
--- src/pages/shop/[id].tsx
+++ src/pages/shop/[id].tsx
@@ -2,6 +2,7 @@ import Breadcrumb from "@/components/modules/Breadcrumb/Breadcrumb";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Layout from "@/components/modules/Layout/Layout";
+import Table from "@/components/templates/shop-[id]/Table/Table";
 const Product = () => {
   const router = useRouter();
 
@@ -9,29 +10,40 @@ const Product = () => {
     <>
       <Breadcrumb href="" text="" />
       <Layout>
-        <div className="flex flex-col gap-4 lg:flex-row">
-          <div className="lg:w-1/3">
-            <div>
-              <Image
-                className="rounded-[0.625rem] border border-[#dee2e6]"
-                width={1000}
-                height={1000}
-                alt=""
-                src="/images/best-product-1.jpg"
-              />
+        <div className="flex flex-col lg:flex-row">
+          <div className="flex flex-col gap-4 lg:w-2/3 lg:flex-row">
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-4 lg:flex-row">
+                <div className="lg:w-1/2">
+                  <div>
+                    <Image
+                      className="rounded-[0.625rem] border border-[#dee2e6]"
+                      width={1000}
+                      height={1000}
+                      alt=""
+                      src="/images/best-product-1.jpg"
+                    />
+                  </div>
+                </div>
+                <div className="lg:w-1/2">
+                  <p className="mb-3 text-2xl font-bold text-darkGray">
+                    Brocoli
+                  </p>
+                  <p className="mb-3 text-lightGray">Category: Vegetables</p>
+                  <p className="mb-3 text-2xl font-bold text-darkGray">3.35$</p>
+                  <p className="mb-3 text-lightGray">
+                    The generated Lorem Ipsum is therefore always free from
+                    repetition injected humour, or non-characteristic words etc.
+                    Susp endisse ultricies nisi vel quam suscipit. Sabertooth
+                    peacock flounder; chain pickerel hatchetfish, pencilfish
+                    snailfish
+                  </p>
+                </div>
+              </div>
+              <Table />
             </div>
           </div>
-          <div className="lg:w-1/3">
-            <p className="mb-3 text-2xl font-bold text-darkGray">Brocoli</p>
-            <p className="mb-3 text-lightGray">Category: Vegetables</p>
-            <p className="mb-3 text-2xl font-bold text-darkGray">3.35$</p>
-            <p className="mb-3 text-lightGray">
-              The generated Lorem Ipsum is therefore always free from repetition
-              injected humour, or non-characteristic words etc. Susp endisse
-              ultricies nisi vel quam suscipit. Sabertooth peacock flounder;
-              chain pickerel hatchetfish, pencilfish snailfish
-            </p>
-          </div>
+          <div className="bg-yellow-100 lg:w-1/3">g</div>
         </div>
       </Layout>
     </>
