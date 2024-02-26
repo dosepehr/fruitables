7c82e61ce3e05cca2d6749809a3b0a0990bcfa13
dosepehr <dosepehr@gmail.com>
2024-02-26 19:36:11 +0330

    add products Table

--- src/components/templates/cart/TableProduct.tsx
+++ src/components/templates/cart/TableProduct.tsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useState, FC } from "react";
 import Image from "next/image";
 import ProductCounter from "../shop-[id]/ProductCounter/ProductCounter";
 import { FaTimes } from "react-icons/fa";
-const TableProduct = () => {
+import { productsType } from "@/types/products.type";
+const TableProduct: FC<productsType> = ({
+  img,
+  name,
+  price,
+  count: productCount,
+}) => {
   const [count, setCount] = useState(0);
 
   const increaseCount: void = (prev: number) => setCount(prev + 1);
@@ -14,30 +20,30 @@ const TableProduct = () => {
   return (
     <>
       <tr className="border-t border-lightGray">
-        <td className="py-4" >
+        <td className="py-4">
           <Image
             className="h-24 w-24 rounded-[0.625rem]"
             width={1000}
             height={1000}
             alt=""
-            src="/images/best-product-1.jpg"
+            src={`http://localhost:9000/images/${img}`}
           />
         </td>
         <td>
-          <p className="text-lightGray">Big Banana</p>
+          <p className="text-lightGray">{name}</p>
         </td>
         <td>
-          <p className="text-lightGray">2.99 $</p>
+          <p className="text-lightGray">{price} $</p>
         </td>
         <td>
           <ProductCounter
-            count={count}
+            count={productCount}
             increaseCount={() => increaseCount(count)}
             decreaseCount={() => decreaseCount(count)}
           />
         </td>
         <td>
-          <p className="text-lightGray">2.99 $</p>
+          <p className="text-lightGray">{productCount * price} $</p>
         </td>
         <td>
           <button className="rounded-full bg-[#f4f6f8] p-2 text-red-500">
