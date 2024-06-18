a2c415f5bcc9440d540a56516b99bcd869f3e939
dosepehr <dosepehr@gmail.com>
2024-06-19 00:06:51 +0330

    fix grid system bug

--- src/components/templates/index/products/components/Product.tsx
+++ src/components/templates/index/products/components/Product.tsx
@@ -3,25 +3,26 @@ import Image from "next/image";
 import AddToCartBtn from "./AddToCartBtn";
 import { productsType } from "@/types/products.type";
 import Link from "next/link";
+
 const Product: FC<productsType> = ({ img, desc, name, price, id }) => {
   return (
     <Link href={`/shop/${id}`}>
-      <div className="rounded-lg border border-mainYellow bg-white pb-4">
-        <div className="h-1/2 overflow-hidden rounded-t-lg">
+      <div className="flex h-full flex-col rounded-lg border border-mainYellow bg-white pb-4">
+        <div className="h-48 overflow-hidden rounded-t-lg">
           <Image
             src={`http://localhost:9000/images/${img}`}
-            className="rounded-t-lg object-cover duration-700 hover:scale-150"
+            className="h-full w-full rounded-t-lg object-cover duration-700 hover:scale-150"
             width={1000}
             height={1000}
-            alt=""
+            alt={name}
           />
         </div>
-        <div className="mx-auto w-[90%] text-center">
+        <div className="mx-auto flex w-[90%] flex-1 flex-col justify-between text-center">
           <div>
             <p className="my-2 text-xl font-semibold text-darkGray">{name}</p>
             <p className="my-2 text-lightGray">{desc.substr(0, 100)}</p>
           </div>
-          <div className="flex items-center justify-around">
+          <div className="mt-4 flex items-center justify-around">
             <p className="text-xl font-semibold text-darkGray">
               $ {price} / Kg
             </p>
--- src/components/templates/index/products/components/ProductsList.tsx
+++ src/components/templates/index/products/components/ProductsList.tsx
@@ -3,16 +3,20 @@ import Product from "./Product";
 import { useQuery } from "@tanstack/react-query";
 import { getProducts } from "@/services";
 import { productsType } from "@/types/products.type";
+
 const ProductsList = () => {
   const { data } = useQuery({
     queryKey: ["products"],
     queryFn: () => getProducts(),
   });
   const products = data?.data;
+
   return (
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-rows-min">
+    <div className="grid auto-rows-fr grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {products?.map((product: productsType) => (
-        <Product key={product.id} {...product} />
+        <div key={product.id} className="flex h-full flex-col">
+          <Product {...product} />
+        </div>
       ))}
     </div>
   );
